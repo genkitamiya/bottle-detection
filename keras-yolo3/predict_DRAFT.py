db7df7049c8d79faaf84077309da3e806f63c1bc
taiki-nakaso <taiki-nakaso@kccs.co.jp>
2020-07-09 16:48:30 +0900

    fix draft

--- keras-yolo3/predict_DRAFT.py
+++ keras-yolo3/predict_DRAFT.py
@@ -9,10 +9,16 @@ from yolo import YOLO
 from PIL import Image
 from time import sleep
 
+# warning処理
 import warnings
-
 warnings.filterwarnings('ignore')
+
 photo_filename = '/tmp/data.jpg'
+
+def is_registered(x):
+    # 登録済みかチェックするやつ
+    return x == 39
+    #return x in range(5)
 
 def shutter():
     photofile = open(photo_filename, 'wb')
@@ -44,6 +50,17 @@ def scan():
 
     return pred, score
 
+def detect_item(pred):
+    """
+    return:
+      会計対象商品: list
+        会計対象となる商品のdictionary内idのlist
+      再読み込みflag: bool
+        Trueの場合再読み込み
+    """
+
+
+    return items, False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
@@ -75,69 +92,21 @@ if __name__ == '__main__':
     # class_names = class_dic['39'][0]
     # prices = class_dic['39'][1]
 
-    if FLAGS.camera:
-        """
-        カメラ検出
-        """
-        checkout_list = []
-        while True:
+    
+    checkout_list = []
+    while True:
+
+        if FLAGS.camera:
+            """
+            カメラ検出
+            """
             key = input('商品をスキャンします。「Enter」を押して下さい')
             pred, score = scan()
 
-            # 未検出ループ
-            if len(pred)==0:
-                key = input('商品を検出しませんでした。再度読み込みますか？ \nはい[y]、いいえ[n]？')
-                if key == 'y':
-                    continue
-
-            # 未登録商品検出ループ
-            if not all([x==39 for x in pred]):
-                key = input('未登録商品を検出しました。再度読み込みますか？ \nはい[y]、いいえ[n]？')
-                if key == 'y':
-                    continue
-                else:
-                    print('未登録商品はお会計されません。')
-                    pred = [x for x in pred if x==39]
-                    pass
-
-            for i, item in enumerate(pred):
-                print('商品番号{} {}の金額は{}'.format(i, class_dic[item][0], class_dic[item][1]))
-
-            # 商品選択
-            flag = True
-            while flag:
-                key = input('お会計を行いたい商品番号を入力してください。(例：0 3 5): ')
-                productid = set(map(int, key.split()))
-                flag = False
-                for i in productid:
-                    if i not in range(len(pred)):
-                        print('商品番号の誤りを検知しました。0-{}の間の番号を入力してください'.format(len(pred)-1))
-                        flag = True
-                        break
-
-
-            checkout_list.extend([pred[x] for x in productid])
-
-            print('買い物カゴに次の商品が入っています。')
-            for item in checkout_list:
-                print('{} ¥{}'.format(class_dic[item][0], class_dic[item][1]))
-
-            # 会計終了プロセス
-            key = input('他の商品もお会計しますか？ \nはい[y]、いいえ[n]？')
-            if key=='y':
-                continue
-            else:
-                break
-
-        print('合計金額は¥{}です。'.format(sum([class_dic[x][1] for x in checkout_list])))
-
-
-    elif FLAGS.file:
-        """
-        データファイル検出
-        """
-        checkout_list = []
-        while True:
+        elif FLAGS.file:
+            """
+            データファイル検出
+            """
             img = input('ファイルパスを入力してください: ')
             try:
                 image = Image.open(img)
@@ -151,52 +120,57 @@ if __name__ == '__main__':
                 pred, score, r_image = yolo.detect_image(image)
                 r_image.save(output_dir + 'result_{}.jpg'.format(file_name.replace('.jpg', '')))
                 plt.imshow(r_image)
-
-            # 未検出ループ
-            if len(pred)==0:
-                key = input('商品を検出しませんでした。再度読み込みますか？ \nはい[y]、いいえ[n]？')
-                if key == 'y':
-                    continue
-
-            # 未登録商品検出ループ
-            if not all([x==39 for x in pred]):
-                key = input('未登録商品を検出しました。再度読み込みますか？ \nはい[y]、いいえ[n]？')
-                if key == 'y':
-                    continue
-                else:
-                    print('未登録商品はお会計されません。')
-                    pred = [x for x in pred if x==39]
-                    pass
-
-            for i, item in enumerate(pred):
-                print('商品番号{} {}の金額は{}'.format(i, class_dic[item][0], class_dic[item][1]))
-
-            # 商品選択
-            flag = True
-            while flag:
-                key = input('お会計を行いたい商品番号を入力してください。(例：0 3 5): ')
-                productid = set(map(int, key.split()))
-                flag = False
-                for i in productid:
-                    if i not in range(len(pred)):
-                        print('商品番号の誤りを検知しました。0-{}の間の番号を入力してください'.format(len(pred)-1))
-                        flag = True
-                        break
-
-
-            checkout_list.extend([pred[x] for x in productid])
-
-            print('買い物カゴに次の商品が入っています。')
-            for item in checkout_list:
-                print('{} ¥{}'.format(class_dic[item][0], class_dic[item][1]))
-
-            # 会計終了プロセス
-            key = input('他の商品もお会計しますか？ \nはい[y]、いいえ[n]？')
-            if key=='y':
+        
+        # 未登録商品検出(消すかも)
+        if not all([is_registered(x) for x in pred]):
+            key = input('未登録商品を検出しました。再度読み込みますか？ \nはい[y]、いいえ[n]？')
+            if key == 'y':
+                continue
+            else:
+                print('未登録商品はお会計されません。')
+                pred = [x for x in pred if is_registered(x)] # 未登録商品を削る
+        
+        # 登録済み商品検出*なし*
+        
+        if len(pred) == 0:
+            key = input('商品を検出しませんでした。再度読み込みますか？ \nはい[y]、いいえ[n]？')
+            if key == 'y':
                 continue
             else:
                 break
 
-        print('合計金額は¥{}です。'.format(sum([class_dic[x][1] for x in checkout_list])))
+        # 登録済み商品検出*あり*
+        else:
+            for i, item in enumerate(pred):
+                print('商品番号{} {}の金額は{}'.format(i, class_dic[item][0], class_dic[item][1]))
+
+            # 商品選択
+            while True:
+                key = input('お会計を行いたい商品番号を入力してください。(例：0 3 5): ')
+                prod_ids = set(map(int, key.split()))
+                # prod_idがpredに対してOutOfIndexでないかチェック
+                if not all([prod_id in range(len(pred)) for prod_id in prod_ids]):
+                    print('商品番号の誤りを検知しました。0-{}の間の番号を入力してください'.format(len(pred)-1))
+                    continue
+                # 全部範囲内ならbreak
+                break
+
+            # pred内のindexから商品IDに変換する
+            items = [pred[x] for x in prid_ids]
+            # カゴに追加
+            checkout_list.extend(items)
+
+        print('買い物カゴに次の商品が入っています。')
+        for item in checkout_list:
+            print('{} ¥{}'.format(class_dic[item][0], class_dic[item][1]))
+
+        # 会計終了プロセス
+        key = input('他の商品もお会計しますか？ \nはい[y]、いいえ[n]？')
+        if key=='y':
+            continue
+        else:
+            break
+
+    print('合計金額は¥{}です。'.format(sum([class_dic[x][1] for x in checkout_list])))
 
     yolo.close_session()
