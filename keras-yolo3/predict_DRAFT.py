1017be896a0c5b2cbf9dd1153830bc6fb2ec792e
genkitamiya <genki02116@gmail.com>
2020-07-13 11:37:26 +0900

    add evaluate.py

--- keras-yolo3/predict_DRAFT.py
+++ keras-yolo3/predict_DRAFT.py
@@ -52,7 +52,7 @@ def scan():
         time = datetime.now().strftime('%Y%m%d%H%M%S')
 
         pred, score, r_image = yolo.detect_image(image)
-        image_path = output_dir + 'result_{}.jpg'.format(file_name.replace('.jpg', '')) 
+        image_path = output_dir + 'result_{}.jpg'.format(file_name.replace('.jpg', ''))
         r_image.save(image_path)
         p = subprocess.Popen(["display", image_path])
         sleep(1)
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     yolo = YOLO()
 
     # 音声ファイル初期化
-    
+
     pygame.mixer.init()
     read_sound = pygame.mixer.Sound("Cash_Register-Beep01-1+6.wav")
     warn_sound = pygame.mixer.Sound("error2.wav")
@@ -178,8 +178,12 @@ if __name__ == '__main__':
                     output_dir = 'output/'
                     _, file_name = ntpath.split(img)
 
+                    start = timer()
                     pred, score, r_image = yolo.detect_image(image)
-                    image_path = output_dir + 'result_{}.jpg'.format(file_name.replace('.jpg', '')) 
+                    end = timer()
+                    print('検出にかかった時間：{:.3f}秒'.format(end - start))
+                    
+                    image_path = output_dir + 'result_{}.jpg'.format(file_name.replace('.jpg', ''))
                     r_image.save(image_path)
                     p = subprocess.Popen(["display", image_path])
                     sleep(1)
